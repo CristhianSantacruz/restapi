b1bf2a5202d96b31201b13234be68b3098a12090
Cristhian Santacruz <crimsant@espol.edu.ec>
2024-08-06 18:11:52 -0500

    fix cors

--- swagger.js
+++ swagger.js
@@ -5,11 +5,11 @@ const doc = {
     "title": 'REST API',
     "description": 'REST API with Express and Firestore'
   },
-  "host": 'localhost:5500',
-  //"host":"restapi-nuh2.onrender.com",
-  //"schemes" : [
-  //  "https"
- // ],
+  //"host": 'localhost:5500',
+  "host":"restapi-nuh2.onrender.com",
+  "schemes" : [
+    "https"
+  ],
   "basePath": "/api",
 };
 
